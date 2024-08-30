c3019c50ceebe71087ba16c7e672dbb2f2979699
anus26 <anusraza268gmail.com>
2024-08-29 21:13:22 -0700

    created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import Button from "./assets/button";
+import "./App.css"
+function App(){
+   const[data, setData]= useState(20);
+   function addcounter() {
+    setData (data + 1)
+   }
+    return(
+        <>
+        <h1>hello world{data}</h1>
+        <Button name="click"/>
+        <Button name="name"/>
+        <button onClick={addcounter}>click</button>
+        </>
+    )
+    
+}
+export default App
